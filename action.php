812a877a2c82be61d98b10fa9e23160352aa4977
joyongjun0702 <42233858+joyongjun0702@users.noreply.github.com>
2019-01-15 19:14:03 +0900

    Update action.php

--- action.php
+++ action.php
@@ -1,3 +1,2 @@
-<?php echo htmlspecialchars($_POST['writer']); ?>씨 안녕하세요.
-제목 : <?php echo (int)$_POST['title']; ?>
-내용 : <?php echo (int)$_POST['content']; ?>
+<?php echo htmlspecialchars($_POST['name']); ?>씨 안녕하세요.
+당신은 <?php echo (int)$_POST['age']; ?>세입니다.
